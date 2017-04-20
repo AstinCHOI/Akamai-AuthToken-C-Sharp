7c328725d2289e1b4ea6c142c78fbe04a7c39cb6
astinchoi <asciineo@gmail.com>
2017-04-20 19:00:17 +0900

    init class setting

---
+++ AuthToken/AuthToken.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+
+namespace Akamai.Auth.Token
+{
+    
+	public class AuthToken
+	{
+		
+	}
+
+    public class AuthTokenBuilder 
+    {
+        
+    }
+
+    public class AuthTokenException
+    {
+        
+    }
+}
---
+++ AuthToken/Test.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+using System;
+
+
+namespace Akamai.Auth.Token
+{
+	[TestFixture()]
+	public class Test
+	{
+		[Test()]
+		public void TestCase()
+		{
+		}
+	}
+}
